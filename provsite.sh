d39244dcfa5f52cd8c8c1bd8d3a98b35a0ebd2ba
Jonathan DeLaigle <delaigle.jonathan@gmail.com>
2011-07-06 13:36:03 -0400

    adjusting site provisioning script for new development layout

--- provsite.sh
+++ provsite.sh
@@ -3,7 +3,7 @@
 
 if [ "$1" == "" ]; then
 echo
-echo "Usage: provision_site.sh \$1){domain name} \$2){database user} \$3){database name}"
+echo "Usage: provision_site.sh \$1){domain name} \$2){database user} \$3){database name} \$4){site status}"
 echo
 exit
 fi
@@ -11,17 +11,18 @@ fi
 domain_name="${1}";
 database_user="${2}";
 database_name="${3}";
-vhost_template='/Users/jonathan/Sites/data/etc/vhosts/vhost_template.txt';
-vhost_dir='/Users/jonathan/Sites/data/etc/vhosts';
+site_status="${4}";
+vhost_template='/Users/jonathan/XAMPP/etc/sites-avaliable/default_template';
+vhost_dir='/Users/jonathan/XAMPP/etc/sites-avaliable';
+vhost_enable='/Users/jonathan/XAMPP/etc/sites-enabled';
 base_logroot_dir='/Users/jonathan/Sites';
 base_docroot_dir='/Users/jonathan/Sites';
 site_root_dir="${base_docroot_dir}/${domain_name}";
-site_logroot_dir="${base_logroot_dir}/${domain_name}/logs";
-site_docroot_dir="${base_docroot_dir}/${domain_name}/htdocs";
-apache_user="nobody";
-base_url='.jonathan.local';
+site_logroot_dir="${base_logroot_dir}/${domain_name}/${site_status}/logs";
+site_docroot_dir="${base_docroot_dir}/${domain_name}/${site_status}/htdocs";
+base_url='.local';
 
-if [[ -z "$domain_name" ]]; then
+if [[ -z "$domain_name" || -z "$site_status" ]]; then
     echo 'You must supply a sitename';
     else
         if [[ -z "$database_user" || -z "$database_name" ]]; then
@@ -69,10 +70,12 @@ if [[ -z "$domain_name" ]]; then
         echo 'Creating host file';
         sudo sh -c "echo \"127.0.0.1 \t ${domain_name}${base_url}\" >> /etc/hosts"
         echo 'Creating vhost';
-        cp $vhost_template ${vhost_dir}/${domain_name}.conf;
-        sed -i '' -e "s:@@@SITENAME@@@:${domain_name}:g" ${vhost_dir}/${domain_name}.conf;
+        cp $vhost_template ${vhost_dir}/${domain_name}_${site_status};
+        sed -i '' -e "s:@@@SITENAME@@@:${domain_name}:g" ${vhost_dir}/${domain_name}_${site_status};
+        sed -i '' -e "s:@@@SITESTATUS@@@:${site_status}:g" ${vhost_dir}/${domain_name}_${site_status};
+        sh -c "ln -s ${vhost_dir}/${domain_name}_${site_status} ${vhost_enable}/${domain_name}_${site_status}"
         echo "Site Complete. Restart Apache and Please visit http://${domain_name}${base_url}/install.php ";
         echo 'Restarting Apache';
-        sudo sh -c '/Applications/xampp/xamppfiles/mampp restartapache' >> /dev/null
+        sudo sh -c 'xampp restartapache' >> /dev/null
         exit 0;
 fi
